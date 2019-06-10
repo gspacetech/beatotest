3e979a468c99e5e746838e2d27fdbdd80ce0656d
nikhil <pgaurav217@gmail.com>
2019-06-11 00:47:40 +0530

    receiver email changed

--- app/src/main/java/com/test/beatoapp/utils/Config.java
+++ app/src/main/java/com/test/beatoapp/utils/Config.java
@@ -4,7 +4,8 @@ public class Config {
 
     public static String SENDER_EMAIL_ID = "beatodemo@gmail.com";
     public static String SENDER_EMAIL_PASSWORD = "test@1234";
-    public static String RECEIVER_EMAIL_ID = "gsp17feb@gmail.com";
+//    public static String RECEIVER_EMAIL_ID = "gsp17feb@gmail.com";
+    public static String RECEIVER_EMAIL_ID = "sanketkumar.biswas@beatoapp.com";
 
     public static String EMAIL_SUB = "Your Beato Test Report";
 
